0d8139f29eb374e6a78742da092e37a5d87aee52
huy-quan-dui <baohoang260803@gmail.com>
2026-06-18 00:56:39 +0700

    update skill, technical, certificates, repair file app,tsx, main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Header from './View/Components/Header/Header'
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Contact from './View/layout/Contact/Contact'
 import { Home } from './View/layout/Home/Home'
 import ScrollToHash from './View/Components/ScrollToHash/ScrollToHash'
@@ -9,22 +9,19 @@ import About from './View/layout/About/About'
 
 function App() {
   return (
-    <HashRouter>
-      <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr auto', minHeight: '100vh', backgroundColor: '#080b14' }}>
-        <Header />
-        <main>
-          {/* Scroll offset compensates for fixed Header */}
-          <ScrollToHash />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </HashRouter>
-
+    <div style={{ display: 'grid', gridTemplateRows: 'auto 1fr auto', minHeight: '100vh', backgroundColor: '#080b14' }}>
+      <Header />
+      <main>
+        {/* Scroll offset compensates for fixed Header */}
+        <ScrollToHash />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
